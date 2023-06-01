9d67b217b8cff1590fa289ecbc784d2837646031
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-06-01 22:45:18 +0300

    mobile is ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,101 +1,52 @@
-/* eslint-disable no-undef */
 'use strict';
 
-const man = document.querySelector('.installation__man');
-const pen = document.querySelector('.installation__pen');
-const ps = document.querySelector('.installation__ps');
-const slots = document.querySelector('.installation__slots');
-const figma = document.querySelector('.installation__figma');
+const man = $('.installation__man');
+const pen = $('.installation__pen');
+const ps = $('.installation__ps');
+const slots = $('.installation__slots');
+const figma = $('.installation__figma');
 
-document.addEventListener('mousemove', function(event) {
+$(document).on('mousemove', function(event) {
   const mouseX = event.clientX;
   const mouseY = event.clientY;
 
-  pen.style.transform = `translate(-${mouseX / 80}px,-${mouseY / 80}px)`;
-  man.style.transform = `translate(-${mouseX / 60}px,-${mouseY / 60}px)`;
-  figma.style.transform = `translate(-${mouseX / 100}px,-${mouseY / 100}px)`;
-  slots.style.transform = `translate(-${mouseX / 50}px,-${mouseY / 50}px)`;
-  ps.style.transform = `translate(-${mouseX / 50}px,-${mouseY / 50}px)`;
+  pen.css('transform', `translate(-${mouseX / 80}px,-${mouseY / 80}px)`);
+  man.css('transform', `translate(-${mouseX / 60}px,-${mouseY / 60}px)`);
+  figma.css('transform', `translate(-${mouseX / 100}px,-${mouseY / 100}px)`);
+  slots.css('transform', `translate(-${mouseX / 50}px,-${mouseY / 50}px)`);
+  ps.css('transform', `translate(-${mouseX / 50}px,-${mouseY / 50}px)`);
 });
 
-// const form = document.querySelector('.form');
-// const page = document.querySelector('.page');
-// const prev = document.querySelector('.prev');
-// const next = document.querySelector('.next');
+const sliderItems = document.querySelectorAll('.slider-item');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
 
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   form.reset();
-// });
+let currentSlide = 0;
 
-// window.onload = function() {
-//   const hash = localStorage.getItem('hash');
+function showSlide(slideIndex) {
+  if (slideIndex < 0) {
+    slideIndex = sliderItems.length - 1;
+  } else if (slideIndex >= sliderItems.length) {
+    slideIndex = 0;
+  }
 
-//   if (hash) {
-//     window.location.hash = hash;
+  sliderItems.forEach((item, index) => {
+    if (index === slideIndex) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
+    }
+  });
 
-//     if (window.location.hash === '#menu') {
-//       localStorage.setItem('hash', window.location.hash);
-//       page.style.overflow = 'hidden';
-//     } else {
-//       page.style.overflow = 'visible';
-//     }
-//   }
-// };
+  currentSlide = slideIndex;
+}
 
-// window.addEventListener('hashchange', () => {
-//   if (window.location.hash === '#menu') {
-//     localStorage.setItem('hash', window.location.hash);
-//     page.style.overflow = 'hidden';
-//   } else {
-//     localStorage.removeItem('hash');
-//     page.style.overflow = 'visible';
-//   }
-// });
+prevButton.addEventListener('click', () => {
+  showSlide(currentSlide - 1);
+});
 
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   form.reset();
-// });
+nextButton.addEventListener('click', () => {
+  showSlide(currentSlide + 1);
+});
 
-// let slideIndex = 1;
-
-// showSlides(slideIndex);
-
-// prev.addEventListener('click', () => {
-//   plusSlides(-1);
-// });
-
-// next.addEventListener('click', () => {
-//   plusSlides(1);
-// });
-
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// function showSlides(n) {
-//   let i;
-//   const slides = document.getElementsByClassName('mySlides');
-//   const dots = document.getElementsByClassName('dot');
-
-//   if (n > slides.length) {
-//     slideIndex = 1;
-//   }
-
-//   if (n < 1) {
-//     slideIndex = slides.length;
-//   }
-
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = 'none';
-//   }
-
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(' active', '');
-//   }
-
-//   slides[slideIndex - 1].style.display = 'block';
-//   // dots[slideIndex - 1].className += ' active';
-// }
+showSlide(currentSlide);
