fd62911b7e7837db48cd1795b7dfa141cb16308f
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-06-01 13:08:59 +0300

    desktop is ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,84 +1,101 @@
 /* eslint-disable no-undef */
 'use strict';
 
-const form = document.querySelector('.form');
-const page = document.querySelector('.page');
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
+const man = document.querySelector('.installation__man');
+const pen = document.querySelector('.installation__pen');
+const ps = document.querySelector('.installation__ps');
+const slots = document.querySelector('.installation__slots');
+const figma = document.querySelector('.installation__figma');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  form.reset();
+document.addEventListener('mousemove', function(event) {
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+
+  pen.style.transform = `translate(-${mouseX / 80}px,-${mouseY / 80}px)`;
+  man.style.transform = `translate(-${mouseX / 60}px,-${mouseY / 60}px)`;
+  figma.style.transform = `translate(-${mouseX / 100}px,-${mouseY / 100}px)`;
+  slots.style.transform = `translate(-${mouseX / 50}px,-${mouseY / 50}px)`;
+  ps.style.transform = `translate(-${mouseX / 50}px,-${mouseY / 50}px)`;
 });
 
-window.onload = function() {
-  const hash = localStorage.getItem('hash');
+// const form = document.querySelector('.form');
+// const page = document.querySelector('.page');
+// const prev = document.querySelector('.prev');
+// const next = document.querySelector('.next');
 
-  if (hash) {
-    window.location.hash = hash;
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   form.reset();
+// });
 
-    if (window.location.hash === '#menu') {
-      localStorage.setItem('hash', window.location.hash);
-      page.style.overflow = 'hidden';
-    } else {
-      page.style.overflow = 'visible';
-    }
-  }
-};
+// window.onload = function() {
+//   const hash = localStorage.getItem('hash');
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    localStorage.setItem('hash', window.location.hash);
-    page.style.overflow = 'hidden';
-  } else {
-    localStorage.removeItem('hash');
-    page.style.overflow = 'visible';
-  }
-});
+//   if (hash) {
+//     window.location.hash = hash;
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  form.reset();
-});
+//     if (window.location.hash === '#menu') {
+//       localStorage.setItem('hash', window.location.hash);
+//       page.style.overflow = 'hidden';
+//     } else {
+//       page.style.overflow = 'visible';
+//     }
+//   }
+// };
 
-let slideIndex = 1;
+// window.addEventListener('hashchange', () => {
+//   if (window.location.hash === '#menu') {
+//     localStorage.setItem('hash', window.location.hash);
+//     page.style.overflow = 'hidden';
+//   } else {
+//     localStorage.removeItem('hash');
+//     page.style.overflow = 'visible';
+//   }
+// });
 
-showSlides(slideIndex);
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   form.reset();
+// });
 
-prev.addEventListener('click', () => {
-  plusSlides(-1);
-});
+// let slideIndex = 1;
 
-next.addEventListener('click', () => {
-  plusSlides(1);
-});
+// showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// prev.addEventListener('click', () => {
+//   plusSlides(-1);
+// });
 
-function showSlides(n) {
-  let i;
-  const slides = document.getElementsByClassName('mySlides');
-  const dots = document.getElementsByClassName('dot');
+// next.addEventListener('click', () => {
+//   plusSlides(1);
+// });
 
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
+// function showSlides(n) {
+//   let i;
+//   const slides = document.getElementsByClassName('mySlides');
+//   const dots = document.getElementsByClassName('dot');
 
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
 
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '');
-  }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
 
-  slides[slideIndex - 1].style.display = 'block';
-  // dots[slideIndex - 1].className += ' active';
-}
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = 'none';
+//   }
+
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(' active', '');
+//   }
+
+//   slides[slideIndex - 1].style.display = 'block';
+//   // dots[slideIndex - 1].className += ' active';
+// }
